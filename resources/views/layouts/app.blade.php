12c4649b3df0d0a67fbc5d96e64aa208a14ac642
Hafiz <m.hafiz1825@gmail.com>
2022-01-29 07:26:04 +0000

    remove gtag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,13 +17,6 @@
 
         gtag('config', 'UA-206865884-3');
         </script>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KLWTFV8');</script>
-        <!-- End Google Tag Manager -->
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -47,10 +40,6 @@
 
     </head>
     <body class="font-sans antialiased">
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLWTFV8"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation', ['catalogs' => \App\Models\Catalog::all()])
 
