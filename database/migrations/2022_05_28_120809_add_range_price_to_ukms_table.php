ec2b7859801a1b83e435a3f66d1497bb608c9321
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-05-28 19:53:03 +0700

    update

--- database/migrations/2022_05_28_120809_add_range_price_to_ukms_table.php
+++ database/migrations/2022_05_28_120809_add_range_price_to_ukms_table.php
@@ -14,7 +14,7 @@ class AddRangePriceToUkmsTable extends Migration
     public function up()
     {
         Schema::table('ukms', function (Blueprint $table) {
-            //
+            $table->integer('price_range');
         });
     }
 
@@ -26,7 +26,7 @@ class AddRangePriceToUkmsTable extends Migration
     public function down()
     {
         Schema::table('ukms', function (Blueprint $table) {
-            //
+            $table->dropColumn('price_range');
         });
     }
 }
